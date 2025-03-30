9a73546659e7b9aafd783156b31885ff4b266fc9
Dominic <dominic.c.diblasio@gmail.com>
2025-03-29 22:08:56 -0700

    Pt.2 Implementation for fetching (ID-based and table-based)
    For Interim progress report!

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // Use OnInit when creating fetched types
+import { TeamProfiles } from '../team-profiles';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-teams',
@@ -7,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './teams.component.css'
 })
 export class TeamsComponent {
+  public team: TeamProfiles | undefined;
 
+  // Construct to also retrieve activatedRoute for fetching
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.getTeamProfile();
+  }
+
+  getTeamProfile() {
+    let id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.http.get<TeamProfiles>(`${environment.baseUrl}api/Teams/${id}`).subscribe({
+      next: result => this.team = result,
+      error: error => console.error(error)
+    }
+    );
+  }
 }
